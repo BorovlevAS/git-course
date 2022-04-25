4a8babc21112b4ba83cb4375f2e73696c7809e11
BorovlevAS <bas@qdes.com.ua>
2022-04-26 00:25:29 +0300

    [+] updated 3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 print("Here is some text!")
 print("and one more string")
 print("and one more string 2")
+print("and one more string 3")
 
 if (__name__ == "__main__"):
     print(__name__)
+    print("and one more string 2")
